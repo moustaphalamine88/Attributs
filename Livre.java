2d4ceefcdf502ec8503fbb77b6e4e3abe3ff35d3
moustapha <moustaphamhtlamien2@gmail.com>
2025-02-11 11:17:29 +0100

    first commit

---
+++ Livre.java
@@ -0,0 +1,13 @@
+package Attribut;
+
+public class Livre {
+    private String titre;
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+}
